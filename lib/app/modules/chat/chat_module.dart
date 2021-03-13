a5ed738f5ba90eaaee16dcc340dc58937806d30b
Marcelo Junior <marcelopugasjunior@gmail.com>
2021-03-12 22:42:51 -0300

    revert versão 2.0

--- lib/app/modules/chat/chat_module.dart
+++ lib/app/modules/chat/chat_module.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'pages/chat/chat_page.dart';
-import 'pages/chat/chat_store.dart';
+import 'chat_page.dart';
+import 'chat_store.dart';
 
-class MainModule extends Module {
+class ChatModule extends Module {
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => ChatStore()),
@@ -10,6 +10,6 @@ class MainModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => ChatPage()),
+    ChildRoute('/home', child: (_, args) => ChatPage()),
   ];
 }
