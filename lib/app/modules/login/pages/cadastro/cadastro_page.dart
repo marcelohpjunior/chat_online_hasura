93b549094c71a020c046fd3ae792dce6c2080dfa
Marcelo Junior <marcelopugasjunior@gmail.com>
2021-03-15 21:50:47 -0300

    Centralização cadastro

--- lib/app/modules/login/pages/cadastro/cadastro_page.dart
+++ lib/app/modules/login/pages/cadastro/cadastro_page.dart
@@ -19,103 +19,105 @@ class CadastroPageState extends State<CadastroPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: TextBorderInput(
-                    onChanged: (text) {
-                      print(text);
-                    },
-                    labelText: "Nome",
-                    hintText: "João",
-                    keyboardType: TextInputType.name,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: TextBorderInput(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: TextBorderInput(
                       onChanged: (text) {
                         print(text);
                       },
-                      labelText: "Sobrenome",
-                      hintText: "Silva",
-                      keyboardType: TextInputType.name),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: TextBorderInput(
-                    onChanged: (text) {
-                      print(text);
-                    },
-                    labelText: "Telefone",
-                    hintText: "ddd + numero",
-                    keyboardType: TextInputType.phone,
+                      labelText: "Nome",
+                      hintText: "João",
+                      keyboardType: TextInputType.name,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: TextBorderInput(
-                    onChanged: (text) {
-                      print(text);
-                    },
-                    labelText: "E-mail",
-                    hintText: "email@email.com",
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 500),
-                  child: TextBorderInput(
-                    onChanged: (text) {
-                      print(text);
-                    },
-                    labelText: "Senha",
-                    hintText: "********",
-                    obscureText: true,
-                    keyboardType: TextInputType.visiblePassword,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40, bottom: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Modular.to.navigate('/chat', replaceAll: true);
-                  },
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).accentColor)),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
                   child: ConstrainedBox(
-                    constraints: BoxConstraints(minWidth: 100),
-                    child: Container(
-                      child: Text(
-                        "Confirmar",
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColorDark,
-                          fontWeight: FontWeight.bold,
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: TextBorderInput(
+                        onChanged: (text) {
+                          print(text);
+                        },
+                        labelText: "Sobrenome",
+                        hintText: "Silva",
+                        keyboardType: TextInputType.name),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: TextBorderInput(
+                      onChanged: (text) {
+                        print(text);
+                      },
+                      labelText: "Telefone",
+                      hintText: "ddd + número",
+                      keyboardType: TextInputType.phone,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: TextBorderInput(
+                      onChanged: (text) {
+                        print(text);
+                      },
+                      labelText: "E-mail",
+                      hintText: "email@email.com",
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 500),
+                    child: TextBorderInput(
+                      onChanged: (text) {
+                        print(text);
+                      },
+                      labelText: "Senha",
+                      hintText: "********",
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, bottom: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Modular.to.navigate('/chat', replaceAll: true);
+                    },
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Theme.of(context).accentColor)),
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(minWidth: 100),
+                      child: Container(
+                        child: Text(
+                          "Confirmar",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColorDark,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        textAlign: TextAlign.center,
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
